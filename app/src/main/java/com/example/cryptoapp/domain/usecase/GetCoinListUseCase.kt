4a3dca2eb18a29d769073465a3953fd983264727
1ntellekt <pitetvasa@gmail.com>
2022-06-29 20:30:05 +0600

    Initial Commit

---
+++ app/src/main/java/com/example/cryptoapp/domain/usecase/GetCoinListUseCase.kt
@@ -0,0 +1,9 @@
+package com.example.cryptoapp.domain.usecase
+
+import com.example.cryptoapp.domain.repository.CoinRepository
+
+class GetCoinListUseCase(private val repository: CoinRepository) {
+
+    operator fun invoke() = repository.getCoinInfoList()
+
+}
